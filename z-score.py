de6b0262ffbe314aeca137c1c590d05b1b457130
CaitlynP123 <74350761+CaitlynP123@users.noreply.github.com>
2022-02-14 20:05:14 +0530

    Add files via upload

--- z-score.py
+++ z-score.py
@@ -1,51 +1,46 @@
-import statistics
-from numpy import mean
-import pandas as pd
-import random 
-import plotly.figure_factory as pf
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
+import random
+import statistics
+import pandas as pd
 
-df = pd.read_csv("C:/Users/C/OneDrive/Desktop/Coding/python/Normal Distribution/medium_data.csv")
+df = pd.read_csv('C:/Users/C/OneDrive/Desktop/Coding/python/Normal Distribution/medium_data.csv')
 data = df['reading_time'].tolist()
-
-popMean = statistics.mean(data)
-popStdDev = statistics.stdev(data)
-
-print('Mean of Population Data = ', popMean)
-print('Standard Deviation of Population Data = ', popStdDev)
 
 def randSetMean(counter):
     dataset = []
     for i in range(0, counter):
-        randomIndex = random.randint(0, len(data)-1)
+        randomIndex = random.randint(0,len(data)-1)
         value = data[randomIndex]
         dataset.append(value)
     mean = statistics.mean(dataset)
-    return(mean)
+    return mean
 
 meanlist = []
+for i in range(0,100):
+    setOfMeans = randSetMean(30)
+    meanlist.append(setOfMeans)
 
-for i in range(0, 1000):
-    setofmeans = randSetMean(100)
-    meanlist.append(setofmeans)
-mean = statistics.mean(meanlist)
-print('Mean of Sampling Distribution = ', mean)
-  
 stdDev = statistics.stdev(meanlist)
-print('Standard Deviation of Sampling Distribution = ', stdDev)
+mean = statistics.mean(meanlist)
 
-std1start, std1end = popMean - stdDev, popMean + stdDev
-std2start, std2end = popMean - (stdDev * 2), popMean + (stdDev * 2)
-std3start, std3end = popMean - (stdDev * 3), popMean + (stdDev * 3)
+print("Mean of sampling distribution:- ",mean)
+print("Standard deviation of sampling distribution:- ", stdDev)
 
-def showDistPlot():
-    fig = pf.create_distplot([meanlist], ['reading_score'], show_hist=False)
-    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode='lines', name='MEAN'))
-    fig.add_trace(go.Scatter(x=[std2end, std2end], y=[0, 0.17], mode='lines', name='Standard Deviation 2 end'))
-    fig.add_trace(go.Scatter(x=[std3end, std3end], y=[0, 0.17], mode='lines', name='Standard Deviation 3 end'))
-    fig.add_trace(go.Scatter(x=[std2start, std2start], y=[0, 0.17], mode='lines', name='Standard Deviation 2 start'))
-    fig.add_trace(go.Scatter(x=[std3start, std3start], y=[0, 0.17], mode='lines', name='Standard Deviation 3 start'))
-    fig.show()
+firstStdDevStart, firstStdDevEnd = mean - stdDev, mean + stdDev
+secondStdDevStart, secondStdDevEnd = mean - (2 * stdDev), mean + (2 * stdDev)
+thirdStdDevStart, thirdStdDevEnd = mean - (3 * stdDev), mean + (3 * stdDev)
 
-showDistPlot()
+meanSample1 = statistics.mean(data)
+print("Mean of sample 1:- ",meanSample1)
 
+fig = ff.create_distplot([meanlist], ["reading time"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.8], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[meanSample1, meanSample1], y=[0, 0.8], mode="lines", name="sample 1 mean"))
+fig.add_trace(go.Scatter(x=[firstStdDevEnd, firstStdDevEnd], y=[0, 0.8], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[secondStdDevEnd, secondStdDevEnd], y=[0, 0.8], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[thirdStdDevEnd, thirdStdDevEnd], y=[0, 0.8], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+z_score = (mean - meanSample1) / stdDev
+print("The z score is = ",z_score)
